5dce4ccca9f28df2974a27c4667f219e4a05fdbe
clevertop <tom@clevertop.dev>
2024-04-29 00:37:49 +1000

    env variables

--- clevertop-dev/utils/supabase/client.ts
+++ clevertop-dev/utils/supabase/client.ts
@@ -2,6 +2,8 @@ import { createBrowserClient } from "@supabase/ssr";
 
 export const createClient = () =>
   createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    //process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    //process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    'https://kdpvrzfvqsfawiffnvbx.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkcHZyemZ2cXNmYXdpZmZudmJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQxMzYzMzgsImV4cCI6MjAyOTcxMjMzOH0.viXJnfviU8VMLmrD2lIrHdvfUUKgLqpX11IhpgLPUIc',
   );
