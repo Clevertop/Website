e2414565ebdb9f874aef8f84ce3a772bf089c976
clevertop <tom@clevertop.dev>
2024-05-02 23:09:09 +1000

    cute favicon and basic site metadata

--- clevertop-dev/app/layout.tsx
+++ clevertop-dev/app/layout.tsx
@@ -8,8 +8,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Tom's Website",
+  description: "meow meow meow meow~ nya! nya! nya!",
 };
 
 export default function RootLayout({
