9bb8c60bffb09dcc8d15e0d5f198754a96b9f542
clevertop <tom@clevertop.dev>
2024-05-02 19:31:43 +1000

    working with supabase

--- clevertop-dev/app/blog2/page.tsx
+++ clevertop-dev/app/blog2/page.tsx
@@ -1,13 +1,24 @@
 import { serialize } from 'next-mdx-remote/serialize'
 import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote/rsc'
-//import { createClient } from '@/utils/supabase/server'
+import { createClient } from '@/utils/supabase/server'
 //import React, { useState } from 'react';
 
 
  
 export default async function RemoteMdxPage() {
+    const supabase = createClient()
+    const { data: posts } = await supabase.from('blog').select()
+    
+    var resSupa = "# If you can see this, then something went wrong"
+    posts?.map((post) => {
+        ( 
+        resSupa = post.content
+        
+        );
+    });
+    
     // MDX text - can be from a local file, database, CMS, fetch, anywhere...
-    const res = await fetch('https://raw.githubusercontent.com/vercel/examples/main/solutions/blog/app/blog/posts/spaces-vs-tabs.mdx')
-    const markdown = await res.text()
-    return <MDXRemote source={markdown} />
+    //const res = await fetch('https://raw.githubusercontent.com/vercel/examples/main/solutions/blog/app/blog/posts/spaces-vs-tabs.mdx')
+    //const markdown = await res.text()
+    return <MDXRemote source={resSupa} />
 }
